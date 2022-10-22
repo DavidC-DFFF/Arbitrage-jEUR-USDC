a8a8ace4daea35d3514bffc81738dfe91dc6c5cd
Remix Workspace <>
2022-10-22 15:59:07 +0200

    Après nettoyage et correction solc 0.6

--- contracts/Kyber-Jarvis.sol
+++ contracts/Kyber-Jarvis.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.6.0;
 
 //import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/solc-0.6/contracts/access/Ownable.sol";
+import "https://github.com/aave/flashloan-box/blob/master/contracts/aave/FlashLoanReceiverBase.sol";
 
 contract arbitragejeur is Ownable {
     address recipientWallet;
